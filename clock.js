a014fbaeb272cbe9ae53d617572b1fb825ec15bb
Brandon P <brandonpegula@gmail.com>
2018-01-19 12:46:17 -0500

    Working with Function

--- clock.js
+++ clock.js
@@ -1,5 +1,18 @@
-var time = new Date();
+var clock = document.getElementById('clock');
+var color = document.getElementById('color');
 
-console.log(time.getHours());
-console.log(time.getMinutes());
-console.log(time.getSeconds());
+function DigitClock(){
+	var time = new Date();
+	var hours = time.getHours().toString();
+	var minutes = time.getMinutes().toString();
+	var seconds = time.getSeconds().toString();
+
+	if hours.length
+
+
+}
+
+
+// console.log(time.getHours());
+// console.log(time.getMinutes());
+// console.log(time.getSeconds());
