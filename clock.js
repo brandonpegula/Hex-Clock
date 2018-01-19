1976d5dca7ebf6378e9048185e260221002194f1
Brandon P <brandonpegula@gmail.com>
2018-01-19 13:20:06 -0500

    Updating Styles

--- clock.js
+++ clock.js
@@ -1,16 +1,34 @@
 var clock = document.getElementById('clock');
 var color = document.getElementById('color');
 
-function DigitClock(){
+function digitClock(){
 	var time = new Date();
 	var hours = time.getHours().toString();
 	var minutes = time.getMinutes().toString();
 	var seconds = time.getSeconds().toString();
 
-	if hours.length
+	if (hours.length < 2) {
+		hours = '0' + hours;
+	}
 
+	if (minutes.length < 2) {
+		minutes = '0' + minutes;
+	}
 
+	if (seconds.length < 2) {
+		seconds = '0' + seconds;
+	}
+
+	var clockString = hours + ':' + minutes + ':' + seconds;
+	var hexcolorString = '#' + hours + minutes + seconds;
+
+	clock.textContent = clockString;
+
+	document.body.style.backgroundColor = hexcolorString;
 }
+
+setInterval(digitClock, 1000);
+
 
 
 // console.log(time.getHours());
